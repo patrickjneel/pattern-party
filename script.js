aaa431b73e3aaae1e90217a4235b99e92773812f
Pat Neel <patrick.neel@colorado.edu>
2018-02-04 15:26:55 -0700

    JS functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const changeClass = (event) => {
   $('.tabs').removeClass('selected')
   $(event.target).addClass('selected')
+  $('.card').addClass('not-shown')
+  $(`.card-${$(event.target).attr('id')}`).removeClass('not-shown')
 }
 
 
