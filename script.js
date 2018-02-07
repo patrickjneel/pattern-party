c1dcb1fee46552d6e4189d9221e84fb0934f4b83
Pat Neel <patrick.neel@colorado.edu>
2018-02-06 21:23:22 -0700

    added some mobile stuff

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const changeClass = (event) => {
   $(event.target).addClass('selected')
   $('.card').addClass('not-shown')
   $(`.card-${$(event.target).attr('id')}`).removeClass('not-shown')
+  $('.minus-button').text('+');
+  $(`.tabs-${$(event.target).attr('id')}`).find('.minus-button').text('-');
 }
 
 
